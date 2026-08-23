05b1fb19eb2dff41391d01550676933017cb9867
uzma631 <auzma867@gmail.com>
2026-08-23 21:53:06 +0500

    second commit

---
+++ M1 L2/understanding the variables.py
@@ -0,0 +1,13 @@
+# Creating different variables
+x = 5
+y = "uzma"
+print(x)
+print(y)
+
+codingal = "codingal_platform"
+print(codingal)
+
+# User Input
+name = input("enter your name: ")
+
+print("\nHello", name, "\nwelcome to codingal")
