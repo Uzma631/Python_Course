6b5cc571a98927ab2dc1f9f093604d7ef95f0595
uzma631 <auzma867@gmail.com>
2026-08-23 21:43:23 +0500

    add commit

--- M1 L1/main.py
+++
@@ -1,4 +0,0 @@
-print("hello World")
-print("My name is uzma")
-print("I love playing badminton")
-print("the result of 5+10= ", 5+10)
---
+++ module1/m1_activity.py
@@ -0,0 +1,19 @@
+#types of data
+#choice = 3.5
+# print(type(choice))
+snack_name =" chips"
+
+
+#arthmetic operations
+#a= 5
+#b=8
+#print(a / b)
+
+#comparison operators
+#num_1 = 100
+3num_2 = 100
+#print(num_1 == num_2)
+#string operations
+
+#swapping values
+
